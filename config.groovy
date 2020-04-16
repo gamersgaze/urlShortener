f09a7a180698ace0c3dba147e63152443f4976dc
QTMHiteshGopani <hitesh.gopani@qmetry.com>
2020-04-16 17:36:47 +0530

    sd

--- config.groovy
+++ config.groovy
@@ -1,3 +1,3 @@
 license="true"    //true | false
-autoDeploy="dev.j8.postgres,qa.j7.mysql"  //dev.j8.postgres,qa.j7.mysql, NA(skip deployment)
+autoDeploy="NA"  //dev.j8.postgres,qa.j7.mysql, NA(skip deployment)
 return this
